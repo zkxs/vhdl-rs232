98c4ecfa12dd25fc4b8f96dc7ea5ff14e2371cbd
Michael Ripley <zkxs00@gmail.com>
2015-12-14 21:55:40 -0600

    Added pinouts

--- rs232.vhd
+++ rs232.vhd
@@ -12,6 +12,25 @@ entity rs232 is
         o8SevenSeg:           out STD_LOGIC_VECTOR(7 downto 0); -- seven seg
         o4SelectSeg:          out STD_LOGIC_VECTOR(3 downto 0); -- seven seg anodes
         
+        i1Rx:                 in  STD_LOGIC;
+        o1Tx:                 out STD_LOGIC;
+        i1RTSin:              in  STD_LOGIC;
+        o1RTSout:             out STD_LOGIC;
+        i1CTSin:              in  STD_LOGIC;
+        o1CTSout:             out STD_LOGIC;
+        
+        o1TxBuffer_in_use:    out STD_LOGIC;
+        o1RxBuffer_in_use:    out STD_LOGIC;
+        o1CTSout_led:         out STD_LOGIC;
+        o1RTSout_led:         out STD_LOGIC;
+        
+        i8switch:             in  STD_LOGIC_VECTOR(7 downto 0);
+        
+        i1BtnStore:           in  STD_LOGIC; -- store switches into TxBuffer
+        i1BtnRead:            in  STD_LOGIC; -- read read data from RxBuffer
+        i1BtnTx:              in  STD_LOGIC; -- transmit data from TxBuffers
+        i1BtnClear:           in  STD_LOGIC; -- clear error status
+        
         debug_clock:          out STD_LOGIC
     );
 end rs232;
@@ -52,12 +71,25 @@ architecture Behavioral of rs232 is
     -- signal declarations
     ----------------------------------------------------------------------------
     
+    signal TxBuffer: STD_LOGIC_VECTOR(7 downto 0);
+    signal RxBuffer: STD_LOGIC_VECTOR(7 downto 0);
+    signal clock_sampling: STD_LOGIC;
+    signal clock_sampling_reset: STD_LOGIC;
     
+    signal CTSout: STD_LOGIC;
+    signal RTSout: STD_LOGIC;
 begin
     ----------------------------------------------------------------------------
     -- glue logic
     ----------------------------------------------------------------------------
+    debug_clock <= clock_sampling;
+    o1CTSout <= CTSout;
+    o1RTSout <= RTSout;
+    o1CTSout_led <= CTSout;
+    o1RTSout_led <= RTSout;
     
+    CTSout <= '0';
+    RTSout <= '0';
     
     ----------------------------------------------------------------------------
     -- process
@@ -72,12 +104,24 @@ begin
     selector: sevenseg_selector
         port map (
             clock       => clock,
-            i4seg4      => "0001",
-            i4seg3      => "0010",
-            i4seg2      => "0100",
-            i4seg1      => "1000",
+            i4seg4      => TxBuffer(7 downto 4),
+            i4seg3      => TxBuffer(3 downto 0),
+            i4seg2      => RxBuffer(7 downto 4),
+            i4seg1      => RxBuffer(3 downto 0),
             o8seg       => o8SevenSeg,
             o4selection => o4SelectSeg
+        );
+        
+    -- sampling clock
+    clock_divider_3ms: clock_divider
+        generic map(
+            divisor => 41667 -- (2400 hz * 10 ns) ^ -1
+        )
+        port map (
+            clock => clock,
+            clear => clock_sampling_reset,
+            enable => '1',
+            Q     => clock_sampling
         );
     
 end Behavioral;
