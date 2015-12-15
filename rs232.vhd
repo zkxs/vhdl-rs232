43ff9aa4ffe8a9bde03310a15343f04b346d3e49
Michael Ripley <zkxs00@gmail.com>
2015-12-15 04:33:49 -0600

    Added transmitting functionality

--- rs232.vhd
+++ rs232.vhd
@@ -84,13 +84,15 @@ architecture Behavioral of rs232 is
     signal RxBuffer: STD_LOGIC_VECTOR(10 downto 0);
     signal TxBuffer_byte: STD_LOGIC_VECTOR(7 downto 0);
     signal RxBuffer_byte: STD_LOGIC_VECTOR(7 downto 0);
+    signal TxBuffer_in_use: STD_LOGIC;
+    signal RxBuffer_in_use: STD_LOGIC;
     
     signal clock_sampling: STD_LOGIC;
     signal clock_sampling_reset: STD_LOGIC;
     signal clock_transmit: STD_LOGIC;
     
     signal CTSout: STD_LOGIC;
-    signal RTSout: STD_LOGIC;
+    signal RTSout: STD_LOGIC := '0';
     
     signal btnStore: STD_LOGIC;
     signal btnRead:  STD_LOGIC;
@@ -100,8 +102,17 @@ architecture Behavioral of rs232 is
     signal btnRead_old:  STD_LOGIC := '0';
     signal btnTx_old:    STD_LOGIC := '0';
     signal btnClear_old: STD_LOGIC := '0';
+    signal Rx_old: STD_LOGIC := '0';
     
     signal xor_cascade_calculate: STD_LOGIC_VECTOR(6 downto 0);
+    
+    signal TxBuffer_index: natural;
+    signal transmitting: STD_LOGIC := '0';
+    
+    signal RxBuffer_index: natural;
+    signal receiving: STD_LOGIC;
+    signal receiving_clock_odd: STD_LOGIC;
+    signal receive_reset: STD_LOGIC := '0';
 begin
     ----------------------------------------------------------------------------
     -- glue logic
@@ -111,9 +122,14 @@ begin
     o1RTSout <= RTSout;
     o1CTSout_led <= CTSout;
     o1RTSout_led <= RTSout;
-    TxBuffer(8 downto 1) <= TxBuffer_byte;
-    RxBuffer_byte <= RxBuffer(8 downto 1);
+    o1TxBuffer_in_use <= TxBuffer_in_use;
+    o1RxBuffer_in_use <= RxBuffer_in_use;
     
+    CTSout <= not RxBuffer_in_use;
+    RTSout <= transmitting;
+    
+    RxBuffer_byte <= RxBuffer(8 downto 1);
+    TxBuffer(8 downto 1) <= TxBuffer_byte;
     TxBuffer(0) <= '0'; -- start bit
     TxBuffer(9) <= not xor_cascade_calculate(0); -- set parity bit
     TxBuffer(10) <= '1'; -- stop bit
@@ -125,45 +141,121 @@ begin
     end generate;
     
     
-    -- zero things i'm not using yet
-    CTSout <= '0';
-    RTSout <= '0';
-    clock_sampling_reset <= '0';
-    o1Tx <= '0';
-    o1TxBuffer_in_use <= '0';
-    o1RxBuffer_in_use <= '0';
+    -- zero things i'm not using yet FIXME
+    RxBuffer_in_use <= '0';
     
     ----------------------------------------------------------------------------
     -- processes
     ----------------------------------------------------------------------------
+    
+    -- transmit process
     process (clock_transmit)
-    begin
+        variable var_transmitting: STD_LOGIC;
+        variable var_TxBuffer_index: natural;
         
-    end process;
-    
-    process (clock_sampling)
-    begin
-        
-    end process;
-    
-    process (clock)
-        variable btnStore_rising: STD_LOGIC;
-        variable btnRead_rising:  STD_LOGIC;
-        variable btnTx_rising:    STD_LOGIC;
         variable btnClear_rising: STD_LOGIC;
+        variable btnStore_rising: STD_LOGIC;
+        variable btnTx_rising:    STD_LOGIC;
+    begin
+        if rising_edge(clock_transmit) then
+            var_transmitting := transmitting;
+            var_TxBuffer_index := TxBuffer_index;
+            btnClear_rising := btnClear and (not btnClear_old);
+            btnStore_rising := btnStore and (not btnStore_old);
+            btnTx_rising    := btnTx    and (not btnTx_old   );
+            btnClear_old <= btnClear;
+            btnStore_old <= btnStore;
+            btnTx_old    <= btnTx   ;
+            
+            
+            if btnClear_rising = '1' then
+                var_transmitting := '0';
+                TxBuffer_in_use <= '0';
+            --elsif btnTx_rising = '1' and TxBuffer_in_use = '1' and var_transmitting = '0' then -- if we want to send
+            elsif btnTx = '1' and TxBuffer_in_use = '1' and var_transmitting = '0' then -- FIXME REMOVE THIS LINE
+                var_transmitting := '1';
+                var_TxBuffer_index := 0;
+            end if;
+            
+            -- if we want to send and it's ok to send
+            --if var_transmitting = '1' and i1CTSin = '0' then -- FIXME REENABLE THIS LINE
+            if var_transmitting = '1' then -- FIXME REMOVE THIS LINE
+                o1Tx <= TxBuffer(var_TxBuffer_index);
+                if var_TxBuffer_index = 10 then
+                    var_transmitting := '0';
+                    --TxBuffer_in_use <= '0'; -- FIXME REENABLE THIS LINE
+                else
+                    var_TxBuffer_index := var_TxBuffer_index + 1;
+                end if;
+            else -- if not transmitting
+                o1Tx <= '1';
+                
+                if btnStore_rising = '1' then
+                    TxBuffer_byte <= i8switch;
+                    TxBuffer_in_use <= '1';
+                end if;
+                
+            end if;
+            
+            transmitting <= var_transmitting;
+            TxBuffer_index <= var_TxBuffer_index;
+        end if;
+    end process;
+    
+    -- receive process
+    process (clock_sampling, receive_reset)
+    begin
+        if receive_reset = '1' then
+            receiving <= '1';
+            receiving_clock_odd <= '0';
+            RxBuffer_index <= 0;
+        elsif rising_edge(clock_sampling) then
+            
+            -- if we need to take a sample
+            if receiving_clock_odd = '1' then
+                
+            end if;
+            
+            receiving_clock_odd <= not receiving_clock_odd;
+        end if;
+    end process;
+    
+    -- process that checks if we need to start receiving
+    process (clock)
+        variable Rx_falling:  STD_LOGIC;
     begin
         if rising_edge(clock) then
-            btnStore_rising := btnStore and (not btnStore_old);
-            btnRead_rising  := btnRead  and (not btnRead_old );
-            btnTx_rising    := btnTx    and (not btnTx_old   );
-            btnClear_rising := btnClear and (not btnClear_old);
-            btnStore_old <= btnStore;
-            btnRead_old  <= btnRead ;
-            btnTx_old    <= btnTx   ;
-            btnClear_old <= btnClear;
+        
+            Rx_falling := (not i1Rx) and Rx_old;
+            Rx_old <= i1Rx;
+        
+            if Rx_falling = '1' and receiving /= '1' then
+                clock_sampling_reset <= '1';
+                receive_reset <= '1';
+            elsif receiving = '1' then
+                clock_sampling_reset <= '0';
+                receive_reset <= '0';
+            end if;
             
-            if btnStore_rising = '1' then
-                TxBuffer_byte <= i8switch;
+        end if;
+    end process;
+    
+    
+    
+    process (btnRead)
+        
+        variable btnRead_rising:  STD_LOGIC;
+        
+        
+    begin
+        if rising_edge(btnRead) then
+            
+            -- btnRead_rising  := btnRead  and (not btnRead_old );
+            -- btnRead_old  <= btnRead ;
+            
+            
+            if transmitting = '0' then
+                
             end if;
         end if;
     end process;
@@ -188,7 +280,7 @@ begin
     -- sampling clock
     clock_divider_sampling: clock_divider
         generic map(
-            divisor => 41667 -- (2400 hz * 10 ns) ^ -1
+            divisor => 20834 -- ((2400 hz * 10 ns) ^ -1) / 2
         )
         port map (
             clock => clock,
